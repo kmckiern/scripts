ee50c4044c68b073d5a4fbb8e86b4a3761f4406c
kmckiern <kmckiern@stanford.edu>
2014-09-25 19:22:55 -0400

    Better

--- opioid/move_best.sh
+++ opioid/move_best.sh
@@ -3,7 +3,7 @@
 cat $1 | while read LINE; do
     LIG=$(echo $LINE | awk -F '/' '{print $1}')
     PREF=$(echo $LINE | awk -F '/' '{print $2}' | awk -F '.' '{print $1}')
-    mkdir -p best/$PREF
+    mkdir -p production/$LIG
     # i think individual dirs are need bc of temp files w non-unique names
-    cp $LINE production/$PREF/$LIG.rst
+    cp $LINE production/$LIG/$LIG.rst
 done

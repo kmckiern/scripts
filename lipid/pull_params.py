8d9df3e5972f6d6ab25c747ab9d1d81dbeb3a7d9
kmckiern <kmckiern@stanford.edu>
2014-10-01 19:24:01 -0700

    maybe works

--- lipid/pull_params.py
+++ lipid/pull_params.py
@@ -1,36 +1,40 @@
 #!/usr/bin/env python
-
 """
-this script pulls all relevant parameter specifications from a forcefield directory in order to generate a ForceBalance .itp file.
+this script pulls all relevant parameter specifications from a forcefield directory in order to generate a ForceBalance .itp file
 
-parser makes lots of assumptions ... will make more general with time.
+parser makes lots of assumptions ... will make more general with time
 """
-
 import argparse
 import os
+import subprocess as subp
 
-parser = argparse.ArgumentParser(description='remove atoms in rf from pdb and write to pdb_o')
+parser = argparse.ArgumentParser(description='parse ff params from atom list file to out file')
 parser.add_argument('--af', type=str, help='file of atoms in system')
 parser.add_argument('--ff_dir', type=str, help='path to ff parameter directory')
 parser.add_argument('--out', type=str, help='name of output parameter file')
 opts = parser.parse_args()
-
-ff_categories = {'defaults': [], 'atomtypes': [], 'bondtypes': [], 'angletypes': [], 'dihedraltypes': [], 'pairtypes': []}
 
 def main():
     # get list of atoms for which we would like to find parameters
     atms = []
     for line in open(opts.af, 'r').readlines():
         atms.append(line.strip())
+    # remove duplicates, and determine now many unique elements
+    uniq_atms = set(atms)
     num_atms = len(atms)
+    
+    # buffer by 3 for parameter type specification and comments
+    parse_length = num_atms + 3
 
-    # read in pdb
-    f = mdtraj.load(opts.pdb) 
+    ff_categories = {'defaults': 3, 'atomtypes': parse_length, 'bondtypes': parse_length, 'angletypes': parse_length, 'dihedraltypes': parse_length, 'pairtypes': parse_length}
 
-    # remove atoms in rm
-    atoms_to_keep = [a.index for a in f.topology.atoms if a.name not in rm]
-    f.restrict_atoms(atoms_to_keep)
-    f.save(opts.pdb_o)
+    path = opts.ff_dir
+
+    # iterate over parameter categories
+    p_cats = ff_categories.keys()
+    for p_type in p_cats:
+        data = subp.Popen(['grep', '-A', str(gg_categories[p_type]), ptype, path + '*'], stdout=subp.PIPE).communicate()[0])
+        print 'mer', data
 
 if __name__ == '__main__':
     main()

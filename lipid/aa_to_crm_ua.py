2f45773cddc1d2ba420e9e5093295f6d742ebdce
kmckiern <kmckiern@stanford.edu>
2014-10-03 15:40:43 -0700

    maybe

--- lipid/aa_to_crm_ua.py
+++ lipid/aa_to_crm_ua.py
@@ -12,12 +12,37 @@ parser.add_argument('--xyz_file', type=str, help='path to coordinate file')
 parser.add_argument('--atm_list', type=str, help='path to list of atoms to keep')
 opts = parser.parse_args()
 
+def get_ls(list_file):
+    a_map = {}
+    # get list of atoms to remove.
+    for line in open(list_file, 'r').readlines():
+        l = line.split()
+        a_map[l[1].strip()] = l[1].strip()
+    return a_map
+
 def main():
-    M = Molecule(opts.xyz_file)
+    # create molecule
+    m = Molecule(opts.xyz_file)
+    # get elements
+    ele = np.array(m.elem)
+    # get atom map
+    am = get_ls(opts.atm_list)
+    # get current atoms
+    a_0 = am.keys()
 
-    atm_ls = get_ls(opts.rf)
+    # get indices of atoms to keep
+    keep = []
+    for itr, e in enumerate(ele):
+        i = itr + 1
+        if e + str(i) in a_0:
+            keep.append(i)
 
-    
+    # throw out some hydrogens
+    xx = m.atom_select(np.where(keep))
+    x = Molecule(opts.xyz_file)
+    x.xyzs = xx.xyzs
+
+    x.write('idk.gro')
 
 if __name__ == '__main__':
     main()

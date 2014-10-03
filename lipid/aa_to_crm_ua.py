e899b2a4521aa8ab836fb9847cd4a28e3b4863ad
kmckiern <kmckiern@stanford.edu>
2014-10-03 15:58:14 -0700

    bleh

--- lipid/aa_to_crm_ua.py
+++ lipid/aa_to_crm_ua.py
@@ -3,22 +3,14 @@
 this script converts an all-atom lipid monomer to a charmm-style united atom representation.
 """
 import argparse
-import os
 from forcebalance.molecule import Molecule
+import numpy as np
 from parse_tools import *
 
 parser = argparse.ArgumentParser(description='parse ff params from atom list file to out file')
 parser.add_argument('--xyz_file', type=str, help='path to coordinate file')
 parser.add_argument('--atm_list', type=str, help='path to list of atoms to keep')
 opts = parser.parse_args()
-
-def get_ls(list_file):
-    a_map = {}
-    # get list of atoms to remove.
-    for line in open(list_file, 'r').readlines():
-        l = line.split()
-        a_map[l[1].strip()] = l[1].strip()
-    return a_map
 
 def main():
     # create molecule
@@ -27,18 +19,12 @@ def main():
     ele = np.array(m.elem)
     # get atom map
     am = get_ls(opts.atm_list)
-    # get current atoms
-    a_0 = am.keys()
+    indx = [int(i)-1 for i in am]
 
-    # get indices of atoms to keep
-    keep = []
-    for itr, e in enumerate(ele):
-        i = itr + 1
-        if e + str(i) in a_0:
-            keep.append(i)
+    print indx
 
     # throw out some hydrogens
-    xx = m.atom_select(np.where(keep))
+    xx = m.atom_select(np.where(indx))
     x = Molecule(opts.xyz_file)
     x.xyzs = xx.xyzs
 

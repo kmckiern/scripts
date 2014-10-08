edb3cfc1c1b0f74f83935e29851254ec3076b954
kmckiern <kmckiern@stanford.edu>
2014-10-07 17:51:30 -0700

    better, but still hacky

--- lipid/aa_to_crm_ua.py
+++ lipid/aa_to_crm_ua.py
@@ -4,31 +4,36 @@ this script converts an all-atom lipid monomer to a charmm-style united atom rep
 """
 import argparse
 from forcebalance.molecule import Molecule
+import mdtraj
 import numpy as np
-from parse_tools import *
 
 parser = argparse.ArgumentParser(description='parse ff params from atom list file to out file')
 parser.add_argument('--xyz_file', type=str, help='path to coordinate file')
-parser.add_argument('--atm_list', type=str, help='path to list of atoms to keep')
+parser.add_argument('--out_xyz', type=str, help='path to list of atoms to keep')
 opts = parser.parse_args()
+
+tf = 'temp.pdb'
+c_strip = ['C18', 'C19', 'C20', 'C21', 'C22', 'C23', 'C24', 'C25', 'C26', 'C27', 'C28', 'C29', 'C30', 'C31', 'C37', 'C38', 'C39', 'C40', 'C41', 'C42', 'C43', 'C44', 'C45', 'C46', 'C47', 'C48', 'C49', 'C50']
+op = 'out.pdb'
 
 def main():
     # create molecule
     m = Molecule(opts.xyz_file)
-    # get elements
-    ele = np.array(m.elem)
-    # get atom map
-    am = get_ls(opts.atm_list)
-    indx = [int(i)-1 for i in am]
-
-    print indx
-
+    # write to pdb
+    m.write(tf)
+    # read into mdtraj
+    p = mdtraj.load(tf)
+    # get list of hydrogens to remove
+    c_ndx = [i.index for i in p.topology.atoms if i.name in c_strip]
+    tail_atms = [y for (x, y) in m.Data['bonds'] if x in c_indx]
+    tail_h = [i for i in tail_atms if i not in c_ndx]
     # throw out some hydrogens
-    xx = m.atom_select(np.where(indx))
-    x = Molecule(opts.xyz_file)
-    x.xyzs = xx.xyzs
-
-    x.write('idk.gro')
+    atoms_to_keep = [b.index for b in t.topology.atoms if b.index not in just_h]
+    p.restrict_atoms(atoms_to_keep)
+    p.save(op)
+    # hella circular 
+    x = Molecule(op)
+    x.write(opts.out_xyz)
 
 if __name__ == '__main__':
     main()

6d2deab9f094c5177ef24721845023a196a1ffa5
kmckiern <kmckiern@stanford.edu>
2014-10-15 11:57:57 -0700

    add ter cards to multiple files

--- opioid/add_ter_oxy.sh
+++ opioid/add_ter_oxy.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
-# sed -i '.bak' '/HH33 NME/a \
-# TER\
-# '  $1
+STDIN=( ${@} )
 
-sed -i '.bak' '/OXT ILE/a \
-TER\
-'  $1
-
-sed -i '.bak' '/HO1 CHL/a \
-TER\
-'  $1
-
-sed -i '.bak' '/H18T OL/a \
-TER\
-'  $1
-
-# for NAL pdbs
-sed -i '.bak' '/H23 NAL/a \
-TER\
-'  $1
-
-# for OXY pdbs
-sed -i '.bak' '/O1  OXY/a \
-TER\
-'  $1
-
-sed -i '.bak' '/Cl- Cl-/a \
-TER\
-'  $1
-
-sed -i '.bak' '/H2  WAT/a \
-TER\
-'  $1
+for i in "${STDIN[@]}"; do
+    # sed -i '.bak' '/HH33 NME/a \
+    # TER\
+    # '  $i
+    
+    sed -i '.bak' '/OXT ILE/a \
+    TER\
+    '  $i
+    
+    sed -i '.bak' '/HO1 CHL/a \
+    TER\
+    '  $i
+    
+    sed -i '.bak' '/H18T OL/a \
+    TER\
+    '  $i
+    
+    # for NAL pdbs
+    sed -i '.bak' '/H23 NAL/a \
+    TER\
+    '  $i
+    
+    # for OXY pdbs
+    sed -i '.bak' '/O1  OXY/a \
+    TER\
+    '  $i
+    
+    sed -i '.bak' '/Cl- Cl-/a \
+    TER\
+    '  $i
+    
+    sed -i '.bak' '/H2  WAT/a \
+    TER\
+    '  $i
+done

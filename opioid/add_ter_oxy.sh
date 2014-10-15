61c60e2c113b64424912cbf23abfa4751913cb03
kmckiern <kmckiern@stanford.edu>
2014-10-15 12:09:30 -0700

    add description

--- opioid/add_ter_oxy.sh
+++ opioid/add_ter_oxy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# for some reason amber requires ter cards in its pdbs
+# however, vmd writes pdbs without ter cards
+# this reinserts the ter cards
+# it may not be perfect as flags were determined by eye
+
 STDIN=( ${@} )
 
 for i in "${STDIN[@]}"; do

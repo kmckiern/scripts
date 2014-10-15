b5c01f17d8e0469cdfe844f6954198f36563a1d6
kmckiern <kmckiern@stanford.edu>
2014-10-15 11:51:54 -0700

    all flags listed

--- opioid/add_ter_oxy.sh
+++ opioid/add_ter_oxy.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
-sed -i '.bak' '/H2  WAT X/a \
+# sed -i '.bak' '/HH33 NME/a \
+# TER\
+# '  $1
+
+sed -i '.bak' '/OXT ILE/a \
+TER\
+'  $1
+
+sed -i '.bak' '/HO1 CHL/a \
+TER\
+'  $1
+
+sed -i '.bak' '/H18T OL/a \
+TER\
+'  $1
+
+# for NAL pdbs
+sed -i '.bak' '/H23 NAL/a \
+TER\
+'  $1
+
+# for OXY pdbs
+sed -i '.bak' '/O1  OXY/a \
+TER\
+'  $1
+
+sed -i '.bak' '/Cl- Cl-/a \
+TER\
+'  $1
+
+sed -i '.bak' '/H2  WAT/a \
 TER\
 '  $1

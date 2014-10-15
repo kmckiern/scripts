98dac88524eb1d8d2f56511407bf955072443c30
kmckiern <kmckiern@stanford.edu>
2014-10-15 12:01:16 -0700

    cleaner

--- opioid/add_ter_oxy.sh
+++ opioid/add_ter_oxy.sh
@@ -3,37 +3,33 @@
 STDIN=( ${@} )
 
 for i in "${STDIN[@]}"; do
-    # sed -i '.bak' '/HH33 NME/a \
-    # TER\
-    # '  $i
-    
-    sed -i '.bak' '/OXT ILE/a \
+    sed -i '.bak' '/OXT ILE/a\
     TER\
     '  $i
     
-    sed -i '.bak' '/HO1 CHL/a \
+    sed -i '.bak' '/HO1 CHL/a\
     TER\
     '  $i
     
-    sed -i '.bak' '/H18T OL/a \
+    sed -i '.bak' '/H18T OL/a\
     TER\
     '  $i
     
     # for NAL pdbs
-    sed -i '.bak' '/H23 NAL/a \
+    sed -i '.bak' '/H23 NAL/a\
     TER\
     '  $i
     
     # for OXY pdbs
-    sed -i '.bak' '/O1  OXY/a \
+    sed -i '.bak' '/O1  OXY/a\
     TER\
     '  $i
     
-    sed -i '.bak' '/Cl- Cl-/a \
+    sed -i '.bak' '/Cl- Cl-/a\
     TER\
     '  $i
     
-    sed -i '.bak' '/H2  WAT/a \
+    sed -i '.bak' '/H2  WAT/a\
     TER\
     '  $i
 done

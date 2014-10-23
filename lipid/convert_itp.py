d465bf44f9235a2b835911d7d9bcbe41154f019a
kmckiern <kmckiern@stanford.edu>
2014-10-23 16:02:55 -0700

    pdbs are good

--- lipid/convert_itp.py
+++ lipid/convert_itp.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 """
+script for converting files between models
+
+mostly written for the following two:
 g53a6b := g
 c36ua := c
 
@@ -21,8 +24,13 @@ import sys
 
 parser = argparse.ArgumentParser(description='parse ff params from atom list file to out file')
 parser.add_argument('--map_file', type=str, help='name of file with atom map specifications')
-parser.add_argument('--model_i', type=str, help='initial model')
-parser.add_argument('--model_f', type=str, help='final model')
+parser.add_argument('--model_i', type=str, help='initial model name')
+parser.add_argument('--model_f', type=str, help='final model name')
+parser.add_argument('--structure_file', action='store_true', help='convert pdb or gro file')
+parser.add_argument('--itp', action='store_true', help='convert itp file')
+parser.add_argument('--f_i', type=str, help='initial model file')
+parser.add_argument('--f_f', type=str, help='initial model file', default=None)
+parser.add_argument('--write_out', type=str, help='name for converted file')
 opts = parser.parse_args()
 
 def model_dict(am, mdl_i, mdl_f):
@@ -30,6 +38,7 @@ def model_dict(am, mdl_i, mdl_f):
     for i, line in enumerate(am):
         l = line.split()
         if i == 0:
+            # if atom name or type dne for mapped model, n/t is kept the same (eg hg hydrogens)
             if l[0] == mdl_i and l[3] == mdl_f:
                 i_ndx = (1,2)
                 f_ndx = (-2,-1)
@@ -46,13 +55,31 @@ def model_dict(am, mdl_i, mdl_f):
         cat[l[i_ndx[1]]] = l[f_ndx[1]]
     return li, lf, ca, cat
 
+def map_sf(in_file, atomname_map, out_file):
+    old_keys = atomname_map.keys()
+    with open(in_file, "r") as template:
+        lines = template.readlines()
+    with open(out_file, "w") as of:
+        for line in lines:
+            l = line.split()
+            if len(l) > 1:
+                if l[1] in old_keys:
+                    l[1] = atomname_map[l[1]]
+                    of.write("\t" + "\t".join(l) + "\n")
+                else:
+                    of.write(line)
+            else:
+                of.write(line)
+
 def main():
     # get atom type and name map
     atom_map = get_ls(opts.map_file)
-    # build description dictionary for each model
+    # build description dictionary for each model, as well as cross model reference
     mi, mf, cross_an, cross_at = model_dict(atom_map, opts.model_i, opts.model_f)
 
-    # build map between models
+    # pdb or gro conversion just maps atom names (not types)
+    if opts.structure_file:
+        map_sf(opts.f_i, cross_an, opts.write_out)
 
 if __name__ == '__main__':
     main()

b92860ea9421d80a2743972174c83e6ef58f4e52
kmckiern <kmckiern@stanford.edu>
2014-10-28 14:03:37 -0700

    generic python template(s)

---
+++ py_temp/blank_arg.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+describe the script hurr
+"""
+
+import argparse
+import os
+import subprocess as subp
+import numpy as np
+
+parser = argparse.ArgumentParser(description='desc')
+parser.add_argument('--str_arg', type=str, help='')
+parser.add_argument('--bool_arg', action='store_true', help='')
+args = parser.parse_args()
+
+def main():
+
+
+if __name__ == '__main__':
+    main()

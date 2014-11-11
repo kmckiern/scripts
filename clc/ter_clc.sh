e39767bbaa5609848180e3f2b94afd9ae060b270
kmckiern <kmckiern@stanford.edu>
2014-11-10 23:29:07 -0800

    woo

---
+++ clc/ter_clc.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# for some reason amber requires ter cards in its pdbs
+# however, vmd writes pdbs without ter cards
+# this reinserts the ter cards
+# it may not be perfect as flags were determined by eye
+
+STDIN=( ${@} )
+
+for i in "${STDIN[@]}"; do
+    # delete random existing ter cards
+    sed -i '.bak' '/TER/d' $i
+
+    # delete preface
+    sed -i '.bak' '/TITLE/d' $i
+    sed -i '.bak' '/REMARK/d' $i
+
+    # delete model info
+    sed -i '.bak' '/MODEL/d' $i
+    sed -i '.bak' 's/ENDMDL/END/g' $i
+
+    # end of protein 
+    sed -i '.bak' '/OT2 PRO/a\
+    TER\
+    '  $i
+
+    # lipids
+    sed -i '.bak' '/H18T OL/a\
+    TER\
+    '  $i
+
+    sed -i '.bak' '/O12 PC/a\
+    TER\
+    '  $i
+
+    sed -i '.bak' '/H16T PA/a\
+    TER\
+    '  $i
+
+    # ions
+    sed -i '.bak' '/Cl- Cl-/a\
+    TER\
+    '  $i
+    
+    sed -i '.bak' '/CAL CAL/a\
+    TER\
+    '  $i
+    
+    sed -i '.bak' '/K+  K+/a\
+    TER\
+    '  $i
+    
+    # water
+    sed -i '.bak' '/H2  WAT/a\
+    TER\
+    '  $i
+done

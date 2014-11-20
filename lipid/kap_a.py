d048049b6de2394f7e227ae06e5e7ed00975d0b6
kmckiern <kmckiern@stanford.edu>
2014-11-20 13:39:24 -0800

    python does not like mixed whitespace tab indentation

--- lipid/kap_a.py
+++ lipid/kap_a.py
@@ -16,37 +16,37 @@ nl = 64
 
 # Parses rvc file.
 def parse(input_file, col1, col2):
-	data = open(input_file).readlines()
-	time = []
-	rel1 = []
-	rel2 = []
-	# Read in each line of the input file.
-	for line in data:
-		l = line.split()
-		time.append[float(l[0])]
-		rel1.append[float(l[1])]
-		rel2.append[float(l[2])]
-	time = np.array(time)
-	rel1 = np.array(rel)
-	rel2 = np.array(rel)
-	al = (rel1 * rel2)/64
-	return time, al
+    data = open(input_file).readlines()
+    time = []
+    rel1 = []
+    rel2 = []
+    # Read in each line of the input file.
+    for line in data:
+        l = line.split()
+        time.append[float(l[0])]
+        rel1.append[float(l[1])]
+        rel2.append[float(l[2])]
+    time = np.array(time)
+    rel1 = np.array(rel)
+    rel2 = np.array(rel)
+    al = (rel1 * rel2)/64
+    return time, al
 
 def main():
-	input_file = args.edr
-	col1 = int(args.col)
-	col2 = (col1 + 1)
-	t = args.temp
+    input_file = args.edr
+    col1 = int(args.col)
+    col2 = (col1 + 1)
+    t = args.temp
 
-	time, al = parse(input_file, col1, col2)
+    time, al = parse(input_file, col1, col2)
     if args.save_al:
         np.savetxt('al_ts_%i.xvg' % t, zip(timw, al))
 
-	avg = np.average(al)
-	fluc = np.average(al**2) - avg**2
+    avg = np.average(al)
+    fluc = np.average(al**2) - avg**2
     kap = 1e3 * (2 * k * t / nl) * (avg * 1.0/ fluc)
 
-	print kap
+    print kap
 
 if __name__ == '__main__':
-	main()
+    main()

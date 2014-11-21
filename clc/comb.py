46cf170655e346634369b1b3857bdea168e43ac4
kmckiern <kmckiern@stanford.edu>
2014-11-21 12:45:54 -0800

    redundant... make general in future

---
+++ clc/comb.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import mdtraj
+from mdtraj import Trajectory as t
+import os
+sys.path.append('/home/kmckiern/scripts/py_general/')
+from toolz.py import *
+
+# combine trajectories
+trjs = [f for f in os.listdir('.') if 'equil' in f and 'netcdf' in f] 
+ts = {}
+tn = []
+for i in trjs:
+    num = i.split('.')[0].split('j')[-1]
+    tn.append(num)
+    ts[i] = t.load(i)
+tn = natural_sort(tn)
+z = ts[tn[0]][-1]
+for i in tn[1:]:
+    print 'z: ', z
+    print 'i: ', i
+    z = z + ts[i][-1]
+
+# save combined data
+zp.save_pdb('out.pdb')

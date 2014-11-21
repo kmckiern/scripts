bb3574bc1b0645596ed7a6357e2e50dcf806fb7b
kmckiern <kmckiern@stanford.edu>
2014-11-21 13:37:26 -0800

    working

--- clc/comb.py
+++ clc/comb.py
@@ -7,15 +7,12 @@ from toolz import *
 
 # combine trajectories
 trjs = [f for f in os.listdir('.') if 'equil' in f and 'netcdf' in f] 
+trjs = natural_sort(trjs)
 ts = {}
-tn = []
 for i in trjs:
-    num = i.split('.')[0].split('j')[-1]
-    tn.append(num)
     ts[i] = t.load(i, top='clc2_eq.pdb', stride=5)
-tn = natural_sort(tn)
-z = ts[tn[0]][-1]
-for i in tn[1:]:
+z = ts[trjs[0]][-1]
+for i in trjs[1:]:
     print 'z: ', z
     print 'i: ', i
     z = z + ts[i][-1]

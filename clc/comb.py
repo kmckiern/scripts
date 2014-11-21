60752cec8a62a23945da4ad2c9003bc97fed36f3
kmckiern <kmckiern@stanford.edu>
2014-11-21 12:52:17 -0800

    works when py_general is added to pythonpath

--- clc/comb.py
+++ clc/comb.py
@@ -2,9 +2,8 @@
 
 import mdtraj
 from mdtraj import Trajectory as t
-import os
-sys.path.append('/home/kmckiern/scripts/py_general/')
-from toolz.py import *
+import os, sys
+from toolz import *
 
 # combine trajectories
 trjs = [f for f in os.listdir('.') if 'equil' in f and 'netcdf' in f] 
@@ -13,7 +12,7 @@ tn = []
 for i in trjs:
     num = i.split('.')[0].split('j')[-1]
     tn.append(num)
-    ts[i] = t.load(i)
+    ts[i] = t.load(i, top='clc2_eq.pdb', stride=5)
 tn = natural_sort(tn)
 z = ts[tn[0]][-1]
 for i in tn[1:]:

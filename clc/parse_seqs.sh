4713c276023ce4f96463a1a86f98980c709cf0f0
kmckiern <kmckiern@stanford.edu>
2014-12-23 21:43:23 -0700

    skip comments

--- clc/parse_seqs.sh
+++ clc/parse_seqs.sh
@@ -3,11 +3,15 @@
 # parse fasta sequences using a list of pdb names
 # inpdb: list of input pdbs
 # seqs: list of fasta sequences
+# parsed: output file
 
 INPDB=$1
 SEQS=$2
 PARSED=$3
 
 while read LINE; do
-    grep -A 1 $LINE $SEQS >> $PARSED
+    if [[ "$LINE" == ";"* ]]; then
+        continue
+    else
+        grep -A 1 $LINE $SEQS >> $PARSED
 done < $INPDB

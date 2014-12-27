62a1f672086cbec560f4f6bae7103114f58c57c7
kmckiern <kmckiern@stanford.edu>
2014-12-27 14:27:34 -0700

    debugging

--- clc/ms_to_mdlr.py
+++ clc/ms_to_mdlr.py
@@ -5,26 +5,29 @@
 import subprocess as subp
 import os
 import argparse
-import numpy as np
-from fmt_path import format_path
+# from toolz import call_cml
 
 parser = argparse.ArgumentParser(description='parse ncbi ms alignment file for modeller')
 parser.add_argument('--a', type=str, help='ncbi alignment file')
 parser.add_argument('--s', type=str, help='sequence file')
 args = parser.parse_args()
 
+# def get_stuff(call):
+#     return call_cml(call).split('\n')[:-1]
+
 # this prob isn't pythonic but w/e.
 def structures(af):
-    entry_data = subp.Popen(['grep', '-n', 'pdb', 'jt.pir'], stdout=subp.PIPE).communicate()[0].split('\n')
+    entry_data = subp.Popen(['grep', '-n', 'pdb', af], stdout=subp.PIPE).communicate()[0].split('\n')[:-1]
     lines = []
     labels = []
     for l in entry_data:
-        lines.append(l.split(':')[0])
+        lines.append(int(l.split(':')[0]))
         labels.append(l.split('|')[3])
+    lines.append(27)
     return lines, labels
 
-def gen_ali(struc, nl):
-    algmt = subp.Popen(['grep', '-A', nl, struc, 'jt.pir'], stdout=subp.PIPE).communicate()[0].split('\n')
+def gen_ali(af, struc, nl):
+    algmt = subp.Popen(['grep', '-A', nl, struc, af], stdout=subp.PIPE).communicate()[0].split('\n')
     algmt.insert(1, 'structure:%s.pdb::::::::' % struc)
     f = open('%s.ali' % struc, 'w')
     for info in algmt[:-1]:
@@ -32,13 +35,12 @@ def gen_ali(struc, nl):
     return f
 
 def main():
-    seq = args.s.readlines()
+    seq = open(args.s, 'r').read()
     ln, pdbs = structures(args.a)
     for i, p in enumerate(pdbs):
-        after = ln[i+1] - ln[i] - 1
-        ali = gen_ali(p, after)
-        with open(ali, 'a') as f:
-            f.write(seq)
+        after = str(ln[i+1] - ln[i] - 1)
+        ali = gen_ali(args.a, p, after)
+        ali.write(seq)
 
 if __name__ == '__main__':
     main()

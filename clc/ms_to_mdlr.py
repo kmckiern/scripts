a1f24fdcd4180391a9c27529145beeecdfdc4fc6
kmckiern <kmckiern@stanford.edu>
2014-12-27 15:26:08 -0700

    a few updates

--- clc/ms_to_mdlr.py
+++ clc/ms_to_mdlr.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
-# parse ncbi alignment file for modeller
+"""
+generate modeller ali files from ncbi ms alignment download and target sequence file
+example usage:
+    >> python /path/to/script/dir/ms_to_mdlr.py --a ../ref/full.pir --s ../ref/P51788.ali --lc
+"""
 
 import subprocess as subp
 import argparse
@@ -9,9 +13,11 @@ from toolz import call_cml
 
 parser = argparse.ArgumentParser(description='parse ncbi ms alignment file for modeller')
 parser.add_argument('--a', type=str, help='ncbi alignment file')
-parser.add_argument('--s', type=str, help='sequence file')
+parser.add_argument('--s', type=str, help='sequence file (to map to alignment structures)')
+parser.add_argument('--lc', action='store_true', help='write pdb info in lowercase')
 args = parser.parse_args()
 
+# mostly for calling bash
 def get_stuff(call):
      return call_cml(call).split('\n')[:-1]
 
@@ -23,12 +29,16 @@ def structures(af):
     for l in entry_data:
         lines.append(int(l.split(':')[0]))
         labels.append(l.split('|')[3])
+    # need file length for final entry
     last = get_stuff('wc %s' % af)
     lines.append(int(last[0].split()[0])+1)
     return lines, labels
 
+# write structures to an ali file
 def gen_ali(af, struc, nl):
     algmt = get_stuff('grep -A %s %s %s' % (nl, struc, af)) 
+    if args.lc:
+        struc = struc.lower()
     algmt.insert(1, 'structure:%s.pdb::::::::' % struc)
     f = open('%s.ali' % struc, 'w')
     for info in algmt:
@@ -41,6 +51,7 @@ def main():
     for i, p in enumerate(pdbs):
         after = str(ln[i+1] - ln[i] - 1)
         ali = gen_ali(args.a, p, after)
+        # append sequence
         ali.write(seq)
 
 if __name__ == '__main__':

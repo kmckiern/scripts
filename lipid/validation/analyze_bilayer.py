651c5c4c0e09a271ad16012367f4f50abc115896
kmckiern <kmckiern@stanford.edu>
2015-04-04 13:12:50 -0700

    test this

--- lipid/validation/analyze_bilayer.py
+++ lipid/validation/analyze_bilayer.py
@@ -3,16 +3,24 @@
 """
 feed in bilayer trj and generate output file with thermodynamic and structural characterization
 example usage:
-    >> python /path/to/script/dir/analyze_bilayer.py --trj /path/to/trj.trr --o ./meh.dat
+    >> python /path/to/script/dir/analyze_bilayer.py --trj /path/to/trj.trr --o meh.dat --xyz tp/
 """
 
 import argparse
-import mdtraj
+from subprocess import Popen, PIPE, STDOUT
 
 parser = argparse.ArgumentParser(description='full analysis of bilayer trajectories')
-parser.add_argument('--trj', type=str, help='path to trj')
+parser.add_argument('--prod_dir', type=str, help='path to directory with trj and tpr')
+parser.add_argument('--prod_pref', type=str, help='file preface for gmx files')
+parser.add_argument('--valid_dir', type=str, help='name of directory for gmx output xyz files')
 parser.add_argument('--o', type=str, help='name of output file')
 args = parser.parse_args()
+
+def cl_gmx(command_lst, pipe_args):
+    p = Popen(command_lst, stdout=PIPE, stdin=PIPE, stderr=STDOUT)  
+    p.communicate(input=pipe_args)
+
+"""def parse_gmx(prop):
 
 # area per lipid
 def get_al():
@@ -30,11 +38,20 @@ def get_al():
 def get_fq(): 
 
 # diffusion constant
-def get_dl():
+def get_dl():"""
+
+def get_struc(pdir, ppref, vdir):
+    props = ['Volume', 'Box-X', 'Box-Y']
+    cl_gmx(['g_energy', '-f', pdir + ppref + '.edr', '-xvg', 'no', '-o', vdir + 'struc.xvg'], \
+            props)
 
 def main():
-    trj = args.trj
+    pdir = args.prod_dir
+    ppref = args.prod_pref
+    vdir = args.valid_dir
     out = args.o
+
+    get_struc(pdir, ppref, )
 
 if __name__ == '__main__':
     main()

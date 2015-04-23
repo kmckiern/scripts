c7b872afa735282b343be2a008404d6eb02a7f2d
kmckiern <kmckiern@stanford.edu>
2015-04-23 14:53:05 -0700

    combine torsion and nb terms

--- ff/gmx_f.py
+++ ff/gmx_f.py
@@ -15,7 +15,7 @@ parser.add_argument('--dx', type=float, help='perturbation amount (angstroms)')
 parser.add_argument('--od', type=str, help='output directory')
 args = parser.parse_args()
 
-compare = ['Bond', 'Angle', 'Torsion']#, 'Nb']
+compare = ['Bond', 'Angle', 'Torsion', 'Nb']
 relevant = {'Bond': 0, 'Angle': 1, 'Proper Dih.': 2, 'Improper Dih.': 3, 'LJ-14': 4, 'Coulomb-14': 5, 'LJ (SR)': 6, 'Disper. corr.': 7, 'Coulomb (SR)': 8, 'Coul. recip.': 9, 'Position Rest.': 10}
 torsions = ['Proper Dih.', 'Improper Dih.']
 nonbond = ['LJ-14', 'Coulomb-14', 'LJ (SR)', 'Coulomb (SR)', 'Coul. recip.', 'Position Rest.']
@@ -30,19 +30,25 @@ od = args.od
 
 for fg in compare:
     f = open(od + '/' + fg + '.dat', 'w')
-    if fg == 'Torsions':
-        data = None
+    data = np.zeros(len(pf[:, relevant[torsions[0]]]))
+    ref = 0
+    if fg == 'Torsion':
         for t in torsions:
             data += pf[:, relevant[t] + 1]
+            ref += upf[relevant[t] + 1] 
+    elif fg == 'Nb':
+        for n in nonbond:
+            data += pf[:, relevant[n] + 1]
+            ref += upf[relevant[n] + 1] 
     else:
         data = pf[:, relevant[fg] + 1]
-    until = len(data)
-    for i in range(until / 6):
+        ref = upf[relevant[fg] + 1]
+    for i in range(len(data) / 6):
         # x, y, and z
         for j in [0, 2, 4]:
             if j < 4:
-                f.write(str(FD(upf[relevant[fg] + 1], data[j+(i*6)], data[(j+1)+(i*6)], h)) + ', ')
+                f.write(str(FD(ref, data[j+(i*6)], data[(j+1)+(i*6)], h)) + ', ')
             else:
-                f.write(str(FD(upf[relevant[fg] + 1], data[j+(i*6)], data[(j+1)+(i*6)], h)))
+                f.write(str(FD(ref, data[j+(i*6)], data[(j+1)+(i*6)], h)))
         f.write('\n')
     f.close()

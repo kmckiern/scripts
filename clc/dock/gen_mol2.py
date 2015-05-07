f0ca90508b1d2fbc88242c94e2a6d5d78956cee5
kmckiern <kmckiern@stanford.edu>
2015-05-07 15:30:17 -0700

    cannot figure out how to use argparse w chimera tho

--- clc/dock/gen_mol2.py
+++ clc/dock/gen_mol2.py
@@ -6,19 +6,16 @@ Use chimera to convert a pdb to a mol2
 
 import os
 from chimera import runCommand as rc
-import argparse
+import sys
 
-parser.add_argument('--pdb', type=float, help='pdb to convert')
-parser.add_argument('--rmh', action='store_true', help='remove hydrogens')
-args = parser.parse_args()
+fn = sys.argv[1]
 
-pdb = args.pdb
-
-for fn in pdb:
-    pref = fn.split('.')[0]
-    rc('open ' + fn)
-    if args.rmh:
-        rc('delete H')
+pref = fn.split('.')[0]
+rc('open ' + fn)
+if len(sys.argv) > 2:
+    rc('delete H')
+    rc('write format mol2 0 ' + pref + '_noH.mol2')
+else:
     rc('write format mol2 0 ' + pref + '.mol2')
-    rc('close all')
+rc('close all')
 rc('stop now')

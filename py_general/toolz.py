fac782a307abef47c81c68ef016552755d3ec79e
kmckiern <kmckiern@stanford.edu>
2015-05-12 16:25:15 -0700

    for extracting zip files

--- py_general/toolz.py
+++ py_general/toolz.py
@@ -1,5 +1,6 @@
 import re
 from subprocess import Popen, PIPE, STDOUT
+import zipfile
 
 def natural_sort(l):
     """ From stack overflow: Natural sorting of a list (so 11 comes after 7) """
@@ -12,3 +13,16 @@ def call_cl(command_lst, pipe_args=[]):
     p = Popen(command_lst, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     out, err = p.communicate(input='\n'.join(pipe_args))
     return out
+
+"""
+adapated from:
+http://stackoverflow.com/questions/9431918/
+    extracting-zip-file-contents-to-specific-directory-in-python-2-7
+"""
+def xtract(fn, dest=None):
+    zfile = zipfile.ZipFile(fn)
+    if dest != None:
+        zfile.extractall(dest)
+    else:
+        pth = '/'.join(fn.split('/')[:1])
+        zfile.extractall(pth)

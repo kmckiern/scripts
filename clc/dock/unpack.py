47c5b4057d98c8b85e7e623e4f82a772cbb7927a
kmckiern <kmckiern@stanford.edu>
2015-05-12 18:31:45 -0700

    working on linux

--- clc/dock/unpack.py
+++ clc/dock/unpack.py
@@ -8,12 +8,13 @@ find new ligs closest to a reference
 import os
 import sys
 import argparse
-sys.path.insert(0, '/home/kerimckiernan/Dropbox/scripts/py_general/')
+sys.path.insert(0, '/home/kmckiern/scripts/py_general/')
 from toolz import xtract
 
 parser = argparse.ArgumentParser(description='get proper docking ligs')
+parser.add_argument('--zf', type=str, help='zip file')
 parser.add_argument('--ref', type=str, help='reference ligand')
 args = parser.parse_args()
 
-fn = sys.argv[1]
+fn = args.zf
 xtract(fn)

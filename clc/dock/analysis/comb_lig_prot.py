8b08ed3d44ef1d168d7020542077162d6ddc1313
kmckiern <kmckiern@stanford.edu>
2015-05-21 21:29:39 -0700

    fixes

--- clc/dock/analysis/comb_lig_prot.py
+++ clc/dock/analysis/comb_lig_prot.py
@@ -8,6 +8,7 @@ parser = argparse.ArgumentParser(description='use chimera to combine protein and
 parser.add_argument('--pro', type=str, help='protein pdb')
 parser.add_argument('--lig', type=str, help='directory for output pdbs (have trailing /)')
 parser.add_argument('--sr', type=str, help='script root', default='/home/kmckiern/scripts/')
+parser.add_argument('--clean_lig', action='store_true', help='clean up ligand pdb', default=False)
 args = parser.parse_args()
 
 sr = args.sr
@@ -22,8 +23,12 @@ def main():
     p = args.pro
     l = args.lig
 
-    comb = st + 'clc/dock/analysis/comb_pl.py ' + p + ' ' + l
-    call_cl(comb)
+    if clean_lig:
+        import mdtraj
+        mdtraj.load(l).save_pdb(l)
+
+    comb = sr + 'clc/dock/analysis/comb_pl.py ' + p + ' ' + l
+    call_chimera(comb)
 
 if __name__ == '__main__':
     main()
--- clc/dock/analysis/comb_pl.py
+++ clc/dock/analysis/comb_pl.py
@@ -10,13 +10,13 @@ from chimera import openModels, MSMSModel
 import sys
 
 p = sys.argv[1]
-ppref = p.split('.')[0]
+ppref = p.split('/')[-1].split('.')[0]
 l = sys.argv[2]
 lpref = l.split('.')[0]
 
 rc('open ' + p)
 rc('open ' + l)
-rc('combine all')
-rc('write selected 2 ' + ppref + '_' + lpref + '.pdb')
+rc('combine #0,#1')
+rc('write #2 ' + ppref + '_' + lpref + '.pdb')
 rc('close all')
 rc('stop now')

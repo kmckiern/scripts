e159ba227639102b51f8ba72e12a9413d715a4d9
kmckiern <kmckiern@stanford.edu>
2015-05-26 13:41:19 -0700

    remove middle wrapper

--- clc/hmodel/align_model.py
+++ clc/hmodel/align_model.py
@@ -1,6 +1,10 @@
 import sys
 from copy import copy
+import time
+
 import Pmw, Tkinter
+from chimera import runCommand as rc
+from chimera import openModels, Molecule
 from chimera.extension import manager
 from MultAlignViewer.MAViewer import MAViewer
 from MultAlignViewer.AddSeqDialog import AddSeqDialog
@@ -8,7 +12,6 @@ from MultAlignViewer.prefs import MATRIX, GAP_OPEN, GAP_EXTEND, \
     USE_SS, SS_MIXTURE, SS_SCORES, HELIX_OPEN, STRAND_OPEN, OTHER_OPEN
 from MatchMaker.gui import SSParams
 from MultAlignViewer import ModellerBase
-from chimera import openModels, Molecule
 
 # borrowed from 
 # http://plato.cgl.ucsf.edu/pipermail/chimera-users/2011-July/006573.html
@@ -20,24 +23,49 @@ def findMAVs():
         raise AssertionError("No MAV instances!")
     return mavs
 
-fmav = findMAVs()
-target, template = fmav
-seq = copy(template.seqs[0])
-seq_name = sys.argv[2].split('/')[-1]
+def model(trgt, templ8, od):
+    # open target sequence
+    rc('open ' + trgt)
+    # open template structure, and generate sequence
+    rc('open ' + tmpl8)
+    rc('sequence #0')
+    
+    # MAV objects
+    fmav = findMAVs()
+    target, template = fmav
+    seq = copy(template.seqs[0])
+    seq_name = trgt.split('/')[-1]
+    
+    # align sequences
+    # get template secondary structure matrix
+    nb = Pmw.NoteBook()
+    structPage = nb.add("From Structure")
+    ssParams = SSParams(structPage, template.prefs)
+    kw = {'ssMatrix': ssParams.getMatrix()}
+    # generalize these vars later.  tired of hacking rn.
+    target.alignSeq(seq, displayName=seq_name, 
+        matrix=template.prefs[MATRIX], gapOpenStrand=-18.0, 
+        scoreGap=-1, scoreGapOpen=-12, gapOpenHelix=-18.0, 
+        gapOpenOther=-6.0, gapChar='.', guideSeqs=None,
+        ssFraction=0.3, **kw)
+    
+    # run modeller on alignment
+    ModellerBase.model(template, seq, openModels.list(modelTypes=[Molecule]), '5', 1, 1, 0, **kw)
 
-# align sequences
-# get template secondary structure matrix
-nb = Pmw.NoteBook()
-structPage = nb.add("From Structure")
-ssParams = SSParams(structPage, template.prefs)
-kw = {'ssMatrix': ssParams.getMatrix()}
-# generalize these vars later.  tired of hacking rn.
-target.alignSeq(seq, displayName=seq_name, 
-    matrix=template.prefs[MATRIX], gapOpenStrand=-18.0, 
-    scoreGap=-1, scoreGapOpen=-12, gapOpenHelix=-18.0, 
-    gapOpenOther=-6.0, gapChar='.', guideSeqs=None,
-    ssFraction=0.3, **kw)
+    """
+    while len(openModels.list(modelTypes=[Molecule])) < 2:
+        time.sleep(120)
+    
+    rc('write 1 ' + od + '$number_' + seq_name)
+    rc('close all')
+    rc('stop now')
+    """
 
-# run modeller on alignment
-ModellerBase.model(template, seq, openModels.list(modelTypes=[Molecule]), '5', 1, 
-    1, 0, **kw)
+def main():
+    # read in args
+    _, trgt, tmpl8, od = sys.argv
+    # model target to template
+    model(trgt, templ8, od)
+
+if __name__ == '__main__':
+    main()

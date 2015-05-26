27b6c4f089a75f00110c921a595e3aada693a954
kmckiern <kmckiern@stanford.edu>
2015-05-26 01:42:00 -0700

    reorg and add write capabilities

--- clc/hmodel/hmodel.py
+++ clc/hmodel/hmodel.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python
-
 """
 use chimera for homology modeling!
 example usage:
-    >> chimera --nogui --script "~/scripts/clc/hmodel/hmodel.py \
+    >> chimera --script "~/scripts/clc/hmodel/hmodel.py \
            target_fasta_seq template.pdb path/out.pdb"
 """
-
 import sys
 from chimera import runCommand as rc
 
 # read in args
-target = sys.argv[1]
-templ8 = sys.argv[2]
+_, target, template, od = sys.argv
 
 # open target sequence
 rc('open ' + target)
-
 # open template
 rc('open ' + templ8)
 rc('sequence #0')
 
+# run alignment and modelling script
 rc('open /Users/kerimckiernan/Dropbox/scripts/clc/hmodel/align.py')
 
-#rc('close all')
-#rc('stop now')
+# save models
+rc('sel 1-5')
+rc('write selected ' + od + '$number_' + template)
+rc('close all')
+rc('stop now')

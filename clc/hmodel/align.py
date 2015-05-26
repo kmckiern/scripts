e2555354127cad7907d942fd21b6286959600401
kmckiern <kmckiern@stanford.edu>
2015-05-25 18:28:04 -0700

    working for nw

--- clc/hmodel/align.py
+++ clc/hmodel/align.py
@@ -1,29 +1,3 @@
-def get_class_members(klass):
-    ret = dir(klass)
-    if hasattr(klass,'__bases__'):
-        for base in klass.__bases__:
-            ret = ret + get_class_members(base)
-    return ret
-
-
-def uniq( seq ): 
-    """ the 'set()' way ( use dict when there's no set ) """
-    return list(set(seq))
-
-def get_object_attrs( obj ):
-    # code borrowed from the rlcompleter module ( see the code for Completer::attr_matches() )
-    ret = dir( obj )
-    ## if "__builtins__" in ret:
-    ##    ret.remove("__builtins__")
-
-    if hasattr( obj, '__class__'):
-        ret.append('__class__')
-        ret.extend( get_class_members(obj.__class__) )
-
-        ret = uniq( ret )
-
-    return ret
-
 # borrowed from 
 # http://plato.cgl.ucsf.edu/pipermail/chimera-users/2011-July/006573.html
 def findMAVs():
@@ -37,15 +11,9 @@ def findMAVs():
     return mavs
 
 fmav = findMAVs()
-for mav in fmav:
-    print '--------------------------------------------'
-    print "mav: ", mav.title
-    print "attributes: "
-    for at in get_object_attrs(mav):
-        print at
-
 target = fmav[0]
 template = fmav[1]
 
 # align sequences
-template.addSeqs(target.seqs)
+from copy import copy
+target.alignSeq(copy(template.seqs[0]))

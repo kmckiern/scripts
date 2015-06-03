4b16f6394e7f2e844471d80540f47055911f5a6d
kmckiern <kmckiern@stanford.edu>
2015-06-03 13:57:17 -0700

    works from arbitrary initial pdb topology

--- clc/build/halve_channel.py
+++ clc/build/halve_channel.py
@@ -5,12 +5,13 @@ import numpy as np
 import mdtraj
 
 parser = argparse.ArgumentParser(description='use chimera to combine protein and ligand pdbs')
-parser.add_argument('--pro', type=str, help='protein pdb')
+parser.add_argument('--pdb', type=str, help='system pdb')
 args = parser.parse_args()
 
 def main():
-    pro = args.pro
-    p = mdtraj.load(pro)
+    system = mdtraj.load(args.pdb)
+    pi = system.topology.select('protein')
+    p = system.atom_slice(pi)
 
     na = p.n_atoms
     first = np.arange(na/2)

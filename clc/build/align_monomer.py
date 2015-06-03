27708148aa8b0367309f982c8520e5c2c64e968d
kmckiern <kmckiern@stanford.edu>
2015-06-03 15:11:47 -0700

    more general

--- clc/build/align_monomer.py
+++ clc/build/align_monomer.py
@@ -10,6 +10,7 @@ import mdtraj
 parser = argparse.ArgumentParser(description='align 3nmo')
 parser.add_argument('--pdb', type=str, help='system pdb')
 parser.add_argument('--ref', type=str, help='reference halved dimer pdb')
+parser.add_argument('--trim_ref', action='store_true', help='trim hydrogens from ref pdb?')
 args = parser.parse_args()
 
 def main():
@@ -17,8 +18,9 @@ def main():
     ref = args.pdb
     m = mdtraj.load(monomer)
     r = mdtraj.load(ref)
-    r_not_H = r.topology.select('name != hydrogen')
-    r_noH = r.atom_slice(r_not_h)
+    if args.trim_ref:
+        r_not_H = r.topology.select('name != hydrogen')
+        r = r.atom_slice(r_not_h)
 
     m_sup = m.superpose(r_noH, frame=0, atom_indices=np.arange(m.n_atoms))
     m_sup.save_pdb('aligned_' + monomer)

a08ad8568c24bf9680bfcc619ee4747c3a21e468
kmckiern <kmckiern@stanford.edu>
2015-06-06 15:22:13 -0700

    so charmm gui will recognize all of the atom names

---
+++ trek/build/prep_for_cg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# for some reason amber requires ter cards in its pdbs
+# however, vmd writes pdbs without ter cards
+# this reinserts the ter cards
+# it may not be perfect as flags were determined by eye
+
+STDIN=( ${@} )
+
+for i in "${STDIN[@]}"; do
+    # remove xtal lipids/ligands/non-k ions
+    sed -i '.bak' '/PC1/d' $i
+    sed -i '.bak' '/40D/d' $i
+    sed -i '.bak' '/ CD /d' $i
+
+    # properly specify ions
+    sed -i '.bak' 's/ K  / POT/g' $i
+done

96878bf4b33740ae6c3034e4e37da471fdb7cc6a
kmckiern <kmckiern@stanford.edu>
2015-06-06 16:02:43 -0700

    remove other superfluous compounds

--- trek/build/prep_for_cg.sh
+++ trek/build/prep_for_cg.sh
@@ -12,4 +12,9 @@ for i in "${STDIN[@]}"; do
     sed -i '.bak' '/PC1/d' $i
     sed -i '.bak' '/40D/d' $i
     sed -i '.bak' '/ CD /d' $i
+    sed -i '.bak' '/B40/d' $i
+    sed -i '.bak' '/A40/d' $i
+    sed -i '.bak' '/A405/d' $i
+    sed -i '.bak' '/A408/d' $i
+    sed -i '.bak' '/TRD/d' $i
 done

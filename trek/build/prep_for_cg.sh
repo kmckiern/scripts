1aaedadd53a85817253a0895ff6c12f9d4642221
kmckiern <kmckiern@stanford.edu>
2015-06-06 19:36:21 -0700

    clean up

--- trek/build/prep_for_cg.sh
+++ trek/build/prep_for_cg.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# for some reason amber requires ter cards in its pdbs
-# however, vmd writes pdbs without ter cards
-# this reinserts the ter cards
-# it may not be perfect as flags were determined by eye
+# remove non-protein non-ion molecules from pdb
 
 STDIN=( ${@} )
 

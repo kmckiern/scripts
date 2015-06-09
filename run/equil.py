b3ed98d56ce1d9c2da44605a4d3809029ffa3cdc
kmckiern <kmckiern@stanford.edu>
2015-06-09 12:20:20 -0700

    so simulation functions are callable

--- run/equil.py
+++ run/equil.py
@@ -67,45 +67,49 @@ def dynamix(simulation, ns, temperature, timestep, which_pu, min=False):
     app.PDBFile.writeFile(top, positions, open(of, 'w'))
     return simulation
 
-parser = argparse.ArgumentParser(description='equilibrate structures')
-parser.add_argument('--pdb', type=str, help='system pdb preface')
-args = parser.parse_args()
-systm = args.pdb
-print(systm)
-# load initial parameters and geometry
-prmtop = app.AmberPrmtopFile(systm + '.prmtop')
-inpcrd = app.AmberInpcrdFile(systm + '.inpcrd')
+def main():
+    parser = argparse.ArgumentParser(description='equilibrate structures')
+    parser.add_argument('--pdb', type=str, help='system pdb preface')
+    args = parser.parse_args()
+    systm = args.pdb
+    print(systm)
+    # load initial parameters and geometry
+    prmtop = app.AmberPrmtopFile(systm + '.prmtop')
+    inpcrd = app.AmberInpcrdFile(systm + '.inpcrd')
+    
+    # heating temperatures, 50 K increments
+    t0_0, t1_0, t1_1, t1_2, t1_3, t2_0 = np.arange(50.0, 301.0, 50.0)
+    # timesteps
+    ts0 = 1.0
+    ts1 = 2.0
+    # number of steps for each process
+    minstep = 1000
+    nstep0 = 10000
+    nstep1 = 50000
+    nstep2 = 5000000
+    
+    #### simulations ####
+    ## cpu / 1 fs timestep ##
+    # minimize and 0 to 50 K for 10 ps
+    s0 = dynamix(None, nstep0, t0_0, ts0, 'cpu', min=True)
+    ## gpu / 1 fs timestep ##
+    # heating, 50 to 100 K for 20 ps
+    s1 = dynamix(s0, 2*nstep1, t1_0, ts0, 'gpu')
+    ## gpu / 2 fs timestep ##
+    # heating, 100 to 250 K for 100 ps each
+    # 100-150
+    s2 = dynamix(s1, nstep1, t1_1, ts1, 'gpu')
+    # 150-200
+    s3 = dynamix(s2, nstep1, t1_2, ts1, 'gpu')
+    # 200-250
+    s4 = dynamix(s3, nstep1, t1_3, ts1, 'gpu')
+    # heating, 250 to 300 K for 10 ns
+    # 250-300
+    pre_eq = current_milli_time()
+    s5 = dynamix(s4, nstep2, t2_0, ts1, 'gpu')
+    post_eq = current_milli_time()
+    total_eq, eq_rate = t_diff(pre_eq, post_eq, ts1, nstep2)
+    print('bench: ' + str(eq_rate) + ' hr/ns')
 
-# heating temperatures, 50 K increments
-t0_0, t1_0, t1_1, t1_2, t1_3, t2_0 = np.arange(50.0, 301.0, 50.0)
-# timesteps
-ts0 = 1.0
-ts1 = 2.0
-# number of steps for each process
-minstep = 1000
-nstep0 = 10000
-nstep1 = 50000
-nstep2 = 5000000
-
-#### simulations ####
-## cpu / 1 fs timestep ##
-# minimize and 0 to 50 K for 10 ps
-s0 = dynamix(None, nstep0, t0_0, ts0, 'cpu', min=True)
-## gpu / 1 fs timestep ##
-# heating, 50 to 100 K for 20 ps
-s1 = dynamix(s0, 2*nstep1, t1_0, ts0, 'gpu')
-## gpu / 2 fs timestep ##
-# heating, 100 to 250 K for 100 ps each
-# 100-150
-s2 = dynamix(s1, nstep1, t1_1, ts1, 'gpu')
-# 150-200
-s3 = dynamix(s2, nstep1, t1_2, ts1, 'gpu')
-# 200-250
-s4 = dynamix(s3, nstep1, t1_3, ts1, 'gpu')
-# heating, 250 to 300 K for 10 ns
-# 250-300
-pre_eq = current_milli_time()
-s5 = dynamix(s4, nstep2, t2_0, ts1, 'gpu')
-post_eq = current_milli_time()
-total_eq, eq_rate = t_diff(pre_eq, post_eq, ts1, nstep2)
-print('bench: ' + str(eq_rate) + ' hr/ns')
+if __name__ == '__main__':
+    main()

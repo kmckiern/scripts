5edb9b8ed1bf0c8915a55f057fa04fe689cc3186
kmckiern <kmckiern@stanford.edu>
2015-06-09 13:11:35 -0700

    fixed global variable errors

--- run/equil.py
+++ run/equil.py
@@ -19,11 +19,14 @@ def t_diff(start, finish, ts, steps):
     ns = (ts * steps * 1.0) / (1000000.0)
     return elapsed, elapsed / ns
 # simulation setup and running
-def setup_sim(temperature, timestep, coordinates, which_pu, box, top):
+def setup_sim(prmtop, temperature, timestep, coordinates, which_pu, top, box=None):
     system = prmtop.createSystem(nonbondedMethod=app.PME,
         nonbondedCutoff=1.0*unit.nanometers, constraints=app.HBonds, rigidWater=True,
         ewaldErrorTolerance=0.0005)
-    system.setDefaultPeriodicBoxVectors(*box)
+    # if a pdb is loaded, box vectors do not need to be loaded (included in topology)
+    # this is not the case for an inpcrd, i think
+    if box != None:
+        system.setDefaultPeriodicBoxVectors(*box)
     integrator = mm.LangevinIntegrator(temperature*unit.kelvin, 1.0/unit.picoseconds,
         timestep*unit.femtoseconds)
     integrator.setConstraintTolerance(0.00001)
@@ -37,7 +40,7 @@ def setup_sim(temperature, timestep, coordinates, which_pu, box, top):
         simulation = app.Simulation(top, system, integrator, platform, properties)
     simulation.context.setPositions(coordinates)
     return simulation
-def dynamix(simulation, ns, temperature, timestep, which_pu, min=False):
+def dynamix(simulation, ns, prmtop, temperature, timestep, which_pu, min=False, print_box=False):
     if min:
         positions = inpcrd.positions
         box = inpcrd.getBoxVectors()
@@ -45,14 +48,15 @@ def dynamix(simulation, ns, temperature, timestep, which_pu, min=False):
         simulation = setup_sim(temperature, timestep, positions, which_pu, box, top)
         simulation.minimizeEnergy(maxIterations=minstep)
         positions = simulation.context.getState(getPositions=True).getPositions()
-        of = systm + '_min_' + str(temperature).split('.')[0] + '.pdb'
+        of = systm + '_min.pdb'
         app.PDBFile.writeFile(top, positions, open(of, 'w'))
     positions = simulation.context.getState(getPositions=True).getPositions()
     box = simulation.context.getState().getPeriodicBoxVectors()
-    of = systm + '_eq_' + str(temperature).split('.')[0] + '.pdb'
-    with open(of + '.box', 'w') as file:
-        for item in box:
-            file.write('{}\n'.format(item))
+    of = systm + '_eq_' + str(temperature).split('.')[0] + '_' + str(ns*timestep/1000.0) + '.pdb'
+    if print_box:
+        with open(of + '.box', 'w') as file:
+            for item in box:
+                file.write('{}\n'.format(item))
     top = simulation.topology
     simulation = setup_sim(temperature, timestep, positions, which_pu, box, top)
     simulation.context.setVelocitiesToTemperature(temperature*unit.kelvin)
@@ -91,22 +95,22 @@ def main():
     #### simulations ####
     ## cpu / 1 fs timestep ##
     # minimize and 0 to 50 K for 10 ps
-    s0 = dynamix(None, nstep0, t0_0, ts0, 'cpu', min=True)
+    s0 = dynamix(None, nstep0, prmtop, t0_0, ts0, 'cpu', min=True)
     ## gpu / 1 fs timestep ##
     # heating, 50 to 100 K for 20 ps
-    s1 = dynamix(s0, 2*nstep1, t1_0, ts0, 'gpu')
+    s1 = dynamix(s0, 2*nstep1, prmtop, t1_0, ts0, 'gpu')
     ## gpu / 2 fs timestep ##
     # heating, 100 to 250 K for 100 ps each
     # 100-150
-    s2 = dynamix(s1, nstep1, t1_1, ts1, 'gpu')
+    s2 = dynamix(s1, nstep1, prmtop, t1_1, ts1, 'gpu')
     # 150-200
-    s3 = dynamix(s2, nstep1, t1_2, ts1, 'gpu')
+    s3 = dynamix(s2, nstep1, prmtop, t1_2, ts1, 'gpu')
     # 200-250
-    s4 = dynamix(s3, nstep1, t1_3, ts1, 'gpu')
+    s4 = dynamix(s3, nstep1, prmtop, t1_3, ts1, 'gpu')
     # heating, 250 to 300 K for 10 ns
     # 250-300
     pre_eq = current_milli_time()
-    s5 = dynamix(s4, nstep2, t2_0, ts1, 'gpu')
+    s5 = dynamix(s4, nstep2, prmtop, t2_0, ts1, 'gpu')
     post_eq = current_milli_time()
     total_eq, eq_rate = t_diff(pre_eq, post_eq, ts1, nstep2)
     print('bench: ' + str(eq_rate) + ' hr/ns')

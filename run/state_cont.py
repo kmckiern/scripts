45276e9937fb008914fd877be31d76e72f4e9dd4
kmckiern <kmckiern@stanford.edu>
2015-06-11 02:49:24 -0700

    continue equilibration from xml files

---
+++ run/state_cont.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+from simtk.openmm import app
+import simtk.openmm as mm
+from simtk import unit
+from sys import stdout
+import os
+import time
+import numpy as np
+import argparse
+
+# serializing
+def serializeObject(obj, filename):
+    objfile = open(filename,'w')
+    objfile.write(mm.XmlSerializer.serialize(obj))
+    objfile.close()
+
+parser = argparse.ArgumentParser(description='continue system equil from state xmls')
+parser.add_argument('--pdb', type=str, help='IC pdb')
+parser.add_argument('--nstep', type=int, help='number of steps')
+parser.add_argument('--prev', type=str, help='file specification from which we are resuming')
+parser.add_argument('--save', type=str, help='file specification to which we are saving')
+args = parser.parse_args()
+
+ns = args.nstep
+prev = args.prev
+save_spec = args.save
+
+system = mm.XmlSerializer.deserializeSystem(open('system' + prev + '.xml').read())
+state = mm.XmlSerializer.deserialize(open('state' + prev + '.xml').read())
+integrator = mm.XmlSerializer.deserialize(open('integrator' + prev + '.xml').read())
+
+platform = mm.Platform.getPlatformByName('CUDA')
+properties = {'CudaPrecision': 'mixed'}
+
+pdb = app.PDBFile(args.pdb)
+simulation = app.Simulation(pdb.topology, system, integrator, platform, properties)
+simulation.context.setPositions(state.getPositions())
+simulation.context.setVelocities(state.getVelocities())
+simulation.context.setPeriodicBoxVectors(*state.getPeriodicBoxVectors())
+for key, value in state.getParameters().iteritems():
+    simulation.context.setParameter(key, value)
+
+# save every 1 ns
+simulation.reporters.append(app.DCDReporter('trajectory' + save_spec + '.dcd', 500000))
+simulation.reporters.append(app.StateDataReporter(stdout, 500000, step=True,
+    potentialEnergy=True, temperature=True, progress=True, remainingTime=True,
+    speed=True, totalSteps=ns, separator='\t'))
+
+# run
+simulation.step(ns)
+
+# save
+serializeObject(system, 'system' + save_spec + '.xml')
+serializeObject(integrator,'integrator' + save_spec + '.xml')
+state = simulation.context.getState(getPositions=True, getVelocities=True,
+    getForces=True, getEnergy=True, getParameters=True, enforcePeriodicBox=True)
+serializeObject(state, 'state' + save_spec + '.xml')

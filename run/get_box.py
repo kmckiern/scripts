01fac01a0b3b531c5c21e3f4a6601b26d2058ef0
kmckiern <kmckiern@stanford.edu>
2015-06-11 17:36:04 -0700

    import units

--- run/get_box.py
+++ run/get_box.py
@@ -2,11 +2,17 @@ from __future__ import print_function
 from simtk.openmm import app
 import simtk.openmm as mm
 from simtk import unit
+from simtk.unit import angstrom
 from sys import stdout
 import os
 import time
 import numpy as np
 import argparse
+
+"""
+example usage:
+while read LINE; do cd $LINE; python ~/scripts/run/get_box.py --suff 32ns; cd ..; done < dimer_dirs.dat
+"""
 
 parser = argparse.ArgumentParser(description='get box vectors from state xml')
 parser.add_argument('--suff', type=str, help='state file suffix')
@@ -21,5 +27,5 @@ qvs = [i.value_in_unit(angstrom) for i in box_vecs]
 
 of = 'box_' + suff + '.dat'
 with open(of + '.box', 'w') as file:
-    for item in box:
+    for item in qvs:
         file.write('{}\n'.format(item))

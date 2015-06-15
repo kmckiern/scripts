97e91dfa1227d97ce4c1a997c8ae9a1e934ca2a0
kmckiern <kmckiern@stanford.edu>
2015-06-14 17:38:54 -0700

    same always

--- clc/dock/superpose_ligands.py
+++ clc/dock/superpose_ligands.py
@@ -11,26 +11,24 @@ import numpy as np
 parser = argparse.ArgumentParser(description='superpose molecules over subset of atoms')
 parser.add_argument('--ref', type=str, help='pdb to which we are aligning')
 parser.add_argument('--translate', type=str, help='pdb we are moving during alignment')
-parser.add_argument('--select', type=str, help='atom selection string')
+parser.add_argument('--rselect', type=str, help='atom selection string for ref')
+parser.add_argument('--tselect', type=str, help='atom selection string for translate lig')
 parser.add_argument('--pout', type=str, help='name of output pdb')
 args = parser.parse_args()
 
 def main():
-    # load in reference and molecule being supoerposed
+    # load in reference and molecule being superposed
     ref = args.ref
     r = mdtraj.load(ref)
-    translate = args.trans
+    translate = args.translate
     t = mdtraj.load(translate)
-    sel = args.select
 
     # find subset of atoms to align
-    sub = r.top.select(sel)
-    check = t.top.select(sel)
+    sub = r.top.select(args.tselect)
 
     # align
-    if sub == check:
-        sposed = t.superpose(r, atom_indices=sub)
-        sposed.save_pdb(args.pout)
+    sposed = t.superpose(r, atom_indices=sub)
+    sposed.save_pdb(args.pout)
 
 if __name__ == '__main__':
     main()

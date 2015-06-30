bba5258f35178dd41b90acf5f900a7c00d33c908
kmckiern <kmckiern@stanford.edu>
2015-06-30 15:31:49 -0700

    working

--- lipid/validation/calc_kap_error.py
+++ lipid/validation/calc_kap_error.py
@@ -5,14 +5,14 @@ import numpy as np
 from forcebalance.nifty import mean_stderr
 import IPython
 
-parser = argparse.ArgumentParser(description='for calculating the error of a 
+parser = argparse.ArgumentParser(description='for calculating the error of a \
     nx2 time series')
 parser.add_argument('--ts', type=str, help='time series')
 args = parser.parse_args()
 
 def main():
-    time, data = np.genfromtxt(args.ts)
-    IPython.embed()
+    data = np.genfromtxt(args.ts)
+    print mean_stderr(data[:,1][::100])
 
 if __name__ == '__main__':
 	main()

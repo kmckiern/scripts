4d603aa93463ec33d7805cbd24a49ebd1f2beab0
kmckiern <kmckiern@stanford.edu>
2015-07-02 10:49:02 -0700

    not quite

--- manip_proteins/renumber_membrane.py
+++ manip_proteins/renumber_membrane.py
@@ -15,24 +15,27 @@ mem_res = ['PA', 'PC', 'OL', 'PE']
 def main():
     pdb = args.pdb
     pdb_o = 'edit_' + pdb
-    of = open(pdb_o, 'w')
+    of = open(pdb_o, 'w+')
+    newid = 0
 
     # read in og pdb
-    with open(pdb, "r") as infile:
+    with open(pdb, 'r') as infile:
         og = infile.readlines()
     # parse party
     for line in og:
         l = line.split()
         if len(l) == 11:
             residue = l[3]
-            resid = l[4]
+            resid = int(l[4])
             if residue in mem_res:
                 # if new residue, note index
                 if residue == mem_res[0]:
-                    if len(resids) == 0:
+                    if newid == 0:
                         newid = resid
                     else:
                         newid += 1
-                line.replace(resid, newid)
+                line.replace(str(resid), str(newid))
         of.write(line)
 
+if __name__ == '__main__':
+    main()

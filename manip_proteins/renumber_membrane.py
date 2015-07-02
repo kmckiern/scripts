6a740bbb1228bf1c22c46af820f99e0d9b0f9803
kmckiern <kmckiern@stanford.edu>
2015-07-02 10:42:32 -0700

    because amber is silly

---
+++ manip_proteins/renumber_membrane.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+
+import argparse
+
+parser = argparse.ArgumentParser(description='renumber membrane residues')
+parser.add_argument('--pdb', type=str, help='pdb file')
+args = parser.parse_args()
+
+"""
+POPC: pa, pc, ol
+POPE: pa, pe, ol
+"""
+mem_res = ['PA', 'PC', 'OL', 'PE']
+
+def main():
+    pdb = args.pdb
+    pdb_o = 'edit_' + pdb
+    of = open(pdb_o, 'w')
+
+    # read in og pdb
+    with open(pdb, "r") as infile:
+        og = infile.readlines()
+    # parse party
+    for line in og:
+        l = line.split()
+        if len(l) == 11:
+            residue = l[3]
+            resid = l[4]
+            if residue in mem_res:
+                # if new residue, note index
+                if residue == mem_res[0]:
+                    if len(resids) == 0:
+                        newid = resid
+                    else:
+                        newid += 1
+                line.replace(resid, newid)
+        of.write(line)
+

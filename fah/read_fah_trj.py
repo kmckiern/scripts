058ec353055e91d7906a0bda6572e1c7af7efe28
kmckiern <kmckiern@stanford.edu>
2015-07-11 17:31:37 -0700

    for fah folding simulation analysis

---
+++ fah/read_fah_trj.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+
+import mdtraj
+import numpy as np
+
+parser = argparse.ArgumentParser(description='get rmsd if fah WU to reference')
+parser.add_argument('--top', type=str, help='topology file')
+parser.add_argument('--ref', type=str, help='reference file')
+parser.add_argument('--out', type=str, help='name of output file', default='rmsd.dat')
+args = parser.parse_args()
+
+def main():
+    # read in fah trj
+    t = args.top
+    trj = mdtraj.load('positions.xtc', top=t)
+
+    # strip water and ions from simulation
+    pi = trj.top.select('protein')
+    trj = trj.atom_slice(pi)
+
+    # calculate rmsd of trj to xtal structure
+    ref = mdtraj.load(args.ref)
+    trj.superpose(ref)
+    dists = mdtraj.rmsd(x, ref)
+    
+    # print final rmsd value
+    print dists[-1]
+    # np.save(args.out, dists)
+
+if __name__ == '__main__':
+    main()

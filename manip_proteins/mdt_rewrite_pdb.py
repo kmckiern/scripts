d6f11c955a82465e848af8a8bb384f7e2c71a77d
kmckiern <kmckiern@stanford.edu>
2015-08-12 17:03:08 -0700

    argparse is better

--- manip_proteins/mdt_rewrite_pdb.py
+++ manip_proteins/mdt_rewrite_pdb.py
@@ -1,7 +1,14 @@
 #!/bin/env python
 
-import sys
 import mdtraj
+import argparse
 
-_, p0, pf = sys.argv
-mdtraj.load(p0).save_pdb(pf)
+parser = argparse.ArgumentParser(description='for rewriting pdbs by mdtraj')
+parser.add_argument('--p0', type=str, help='pdb in', default='protein.pdb')
+parser.add_argument('--pf', type=str, help='pdb out', default='out.pdb')
+args = parser.parse_args()
+
+p0 = args.p0
+pf = args.pf
+
+mdtraj.load_pdb(p0).save_pdb(pf)

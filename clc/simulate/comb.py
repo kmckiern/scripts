59af95e0e69546b6e887edc3eda813552d497938
kmckiern <kmckiern@stanford.edu>
2015-08-18 16:06:44 -0700

    fixed bugs

--- clc/simulate/comb.py
+++ clc/simulate/comb.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
+import numpy as np
 import mdtraj
 from mdtraj import Trajectory as t
-import numpy as np
 import os, sys
 import argparse
 
@@ -11,10 +11,10 @@ parser.add_argument('--trj_dir', type=str, help='directory of simulation traject
 parser.add_argument('--trj_ext', type=str, help='simulation trajectory extension', default='netcdf')
 parser.add_argument('--w_ext', type=str, help='written sim trajectory extension', default='dcd')
 parser.add_argument('--top', type=str, help='reference pdb topology')
-parser.add_argument('--stride1', type=int, help='individual trj subsample rate')
-parser.add_argument('--stride2', type=int, help='combined trj subsample rate')
-parser.add_argument('--cut', type=int, help='frames before this index will be cut')
-parser.add_argument('--vs', action='store_true', help='write trj of voltage sensitive residues')
+parser.add_argument('--stride1', type=int, help='individual trj subsample rate', default=0)
+parser.add_argument('--stride2', type=int, help='combined trj subsample rate', default=0)
+parser.add_argument('--cut', type=int, help='frames before this index will be cut', default=0)
+parser.add_argument('--vs', action='store_true', help='write trj of voltage sensitive residues', default=False)
 parser.add_argument('--sr', type=str, help='script root', default='/home/kmckiern/scripts/')
 args = parser.parse_args()
 
@@ -26,7 +26,7 @@ ext_i = args.trj_ext
 ext_o = args.w_ext
 
 # combine trajectories
-trjs = [f for f in os.listdir(args.trj_dir) if ext in f] 
+trjs = [f for f in os.listdir(args.trj_dir) if ext_i in f] 
 trjs = natural_sort(trjs)
 ts = t.load(trjs[0], top=args.top, stride=args.stride1)
 for i in trjs[1:]:
@@ -39,4 +39,4 @@ if args.vs:
     ts = ts.atom_slice(vs)
 
 # save combined data
-ts[args.cut::args.stride2].save_pdb(args.trj_dir + '/out.pdb')
+ts[args.cut::args.stride2].save_pdb(args.trj_dir + '/out' + ext_o)

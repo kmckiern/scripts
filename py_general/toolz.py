b039640752d43af6dc7c766d87e82bd6bbcea9c5
kmckiern <kmckiern@stanford.edu>
2015-10-21 13:13:17 -0700

    need dat cartesian product

--- py_general/toolz.py
+++ py_general/toolz.py
@@ -40,3 +40,21 @@ def xtract(fn, dest=None):
     else:
         pth = '/'.join(fn.split('/')[:-1])
         zfile.extractall(pth)
+
+# via http://stackoverflow.com/questions/1208118/using-numpy-to-build-an-array-of-all-combinations-of-two-arrays
+def cartesian(arrays, out=None):
+    """
+    Generate a cartesian product of input arrays.
+    """
+    arrays = [np.asarray(x) for x in arrays]
+    dtype = arrays[0].dtype
+    n = np.prod([x.size for x in arrays])
+    if out is None:
+        out = np.zeros([n, len(arrays)], dtype=dtype)
+    m = n / arrays[0].size
+    out[:,0] = np.repeat(arrays[0], m)
+    if arrays[1:]:
+        cartesian(arrays[1:], out=out[0:m,1:])
+        for j in xrange(1, arrays[0].size):
+            out[j*m:(j+1)*m,1:] = out[0:m,1:]
+    return out

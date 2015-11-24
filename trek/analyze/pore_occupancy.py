73f0566568a88fe16ecfdcdd421484b76734eec8
kmckiern <kmckiern@stanford.edu>
2015-11-23 23:17:53 -0800

    rough extension

--- trek/analyze/pore_occupancy.py
+++ trek/analyze/pore_occupancy.py
@@ -5,18 +5,17 @@ example usage:
         python pore_occupancy.py --trj whereever/cnv.xtc --top 4xdk.pdb --out sf_h2o_p9761_17_14.dat
 """
 
+import argparse
 import mdtraj
 import numpy as np
-import argparse
-import glob
-from natsort import natsorted
-from msmbuilder.dataset import dataset
 import pandas as pd
-import os.path
+from scipy.stats import multivariate_normal
 import IPython
 
 parser = argparse.ArgumentParser(description='get water and ion occupancy of trek sf')
 parser.add_argument('--tf', type=str, help='trajectory file')
+parser.add_argument('--gaussian', type='store_true', help='gaussian counting', default=False)
+parser.add_argument('--histogram', type='store_true', help='histogram counting', default=False)
 parser.add_argument('--start', type=int, help='start bin for sum window')
 parser.add_argument('--end', type=int, help='end bin for sum window')
 args = parser.parse_args()
@@ -32,37 +31,42 @@ nb = 2 * num_bins
 
 res_indxs = [101, 210, 362, 471]
 
-def init_trj(trajectory, topology):
-    # load trjs
-    traj = mdtraj.load(trajectory, top=topology)
-
-    pi = traj.top.select('protein')
-    traj = traj.superpose(traj, atom_indices=pi)
+# via: 
+# http://stackoverflow.com/questions/25720600/generating-3d-gaussian-distribution-in-python
+def numerical_gaussian(xmin, xmax, ymin, ymax, mu, sigma):
+    # generate numerical grid
+    x, y = np.mgrid[xmin:xmax, ymin:ymax]
+    xy = np.column_stack([x.flat, y.flat])
+    # get gaussian values over grid
+    covariance = np.diag(sigma**2)
+    gz = multivariate_normal.pdf(xy, mean=mu, cov=covariance)
+    gz = g.reshape(x.shape)
+    return gz
     
-    # get reference indices
-    x = traj[0]
+def relevant_indices(trj):
     # CA of each GLY located centerally to each selectivity filter strand
     # +2 is top, -2 is bottom of strand
-    for i in [-2, -1, 1, 2]:
+    for i in [-3, -2, -1, 1, 2, 3]:
         for j in range(4):
             res_indxs.append(res_indxs[j]+i)
     atom_indxs = []
     for ref in res_indxs:
-        atom_indxs += [atom.index for atom in x.top.atoms if ((atom.residue.resSeq == ref) and (atom.name == 'CA'))]
+        atom_indxs += [atom.index for atom in trj.top.atoms if ((atom.residue.resSeq == ref) and (atom.name == 'CA'))]
     ri = np.array(atom_indxs)
     wi = [atom.index for atom in traj.topology.atoms if (atom.residue.is_water and (atom.element.symbol == 'O'))]
     ki = np.array([i.index for i in traj.top.atoms_by_name('K+')])
-    
+    return ri, wi, ki
+
+def sf_bc(traj, ri):
     # filter by z
     ri_qz = traj.xyz[:, ri, -1]
     z_cent = np.average(ri_qz, axis=1)
     z_min = z_cent - cyl_length
     z_max = z_cent + cyl_length
     # filter by xy
-    ri_qxy = traj.xyz[:,ri,:2]
+    ri_qxy = traj.xyz[:, ri, :2]
     xy_cent = np.average(ri_qxy, axis=1)
-
-    return traj, wi, ki, z_min, z_max, xy_cent
+    return z_min, z_max, xy_cent
 
 def discrete_bins(traj, wi, ki, z_min, z_max, xy_cent):
     nf = len(traj)
@@ -71,17 +75,16 @@ def discrete_bins(traj, wi, ki, z_min, z_max, xy_cent):
 
     z_lbls = []
     for ndx, atom_var in enumerate([wi, ki]):
-        zs = traj.xyz[:, atom_var, -1]
         # label above (1), within (0), or below (-1) sf wrt z
+        zs = traj.xyz[:, atom_var, -1]
         z_scale = (zs.T - z_min).T
         z_scale[(z_scale >= 0.0) & (z_scale <= full_l)] = 0
         z_scale[z_scale < 0.0] = -1
         z_scale[z_scale > full_l] = 1
         z_lbls.append(z_scale)
     
-        xys = traj.xyz[:,atom_var,:2]
         # label if within radius of sf (0), or outside (1)
-        # broadcasting is a little weird
+        xys = traj.xyz[:,atom_var,:2]
         sx_scale = (xys[:,:,0].T - xy_cent[:,0]).T
         sy_scale = (xys[:,:,1].T - xy_cent[:,1]).T
         moduli = sx_scale**2 + sy_scale**2
@@ -116,25 +119,35 @@ def write_bins(of, time_series, out_pref):
     
 def main():
     trj_data = open(args.tf).readlines() 
-    for t_ndx, trj in enumerate(trj_data):
-        # label stuff
-        label = trj.strip()
+    for t_ndx, trjd in enumerate(trj_data):
+        # read, load, align
+        t, top = trjd.split()
+        label = t.strip()
+        trj = mdtraj.load(t, top=top)
+        pi = traj.top.select('protein')
+        trj = trj.superpose(trj, atom_indices=pi)
+        x = trj[0]
 
-        # read in and histogram ions and water molecules
-        t = 'trjs/' + label + '.xtc'
-        top = 'tops/p9761_79_12.pdb'
+        ri, wi, ki = relevant_indices(x)
+        # get ions and water in the selectivity filter
+        if args.gaussian:
+            # fit 10 gaussians to SF 
+            gaussians = fit_gaussians()
+            # bin gaussian counts
+            bin_timeseries, zs = gaussian_bins(*gaussians)
 
-        # get trj info
-        trj_geo = init_trj(t, top)
-        # histogram data
-        bin_timeseries, zs = discrete_bins(*trj_geo)
-        # write bin data
+        if args.histogram:
+            # get trj info
+            sfbc = sf_bc(x, ri)
+            # histogram data
+            bin_timeseries, zs = discrete_bins(traj, wi, ki, *sfbc)
+
+        # record bin data
         for ndx, i in enumerate(bin_timeseries):
             if ndx == 0:
                 write_bins(label, zs[0], 'atom_resolved_bins/water_')
             if ndx == 1:
                 write_bins(label, zs[1], 'atom_resolved_bins/k_')
-        # write bin data
         write_bins(label, bin_timeseries, 'bins/b')
         # sum over bins for feature vector of SF occupancy
         first = args.start

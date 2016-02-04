8519a9ce2c9560cbf791491da978f5e161db7efe
kmckiern <kmckiern@stanford.edu>
2016-02-03 19:59:06 -0800

    final

--- trek/analyze/pbcmol_ics.py
+++ trek/analyze/pbcmol_ics.py
@@ -22,19 +22,20 @@ def main():
     for state in dirs:
         frames = df.loc[state]
         for ndx, IC in enumerate(frames):
+            if state != 'ab_2_o_13':
+                continue
             if IC == None:
                 continue
             else:
-                print (state, IC)
                 # prep everything
                 odir = os.path.join('done', 'ICs', state)
+                t_out = os.path.join(odir, str(ndx) + '.gro')
                 if not os.path.isdir(odir):
                     os.makedirs(odir)
                 fr, top = IC
                 trace = fr.split('.xtc')[0]
                 topology = os.path.join(top_pref, top + '.top')
                 trajectory = os.path.join('ICs', state, str(ndx) + '.gro')
-                t_out = os.path.join(odir, str(ndx) + '.gro')
                 # run grompp
                 g_out = os.path.join('tpr', trace + '_' + str(ndx) + '.tpr')
                 run_grompp = os.path.join(gmx_pref, 'grompp') + ' -f files/npt.mdp -c ' \
@@ -43,7 +44,7 @@ def main():
                 # run trjconv
                 run_trjconv = os.path.join(gmx_pref, 'trjconv') + ' -f ' + trajectory \
                     + ' -s ' + g_out + ' -o ' +  t_out + ' -pbc mol -center'
-                call_cl(run_trjconv, pipe_args=[b'Protein', b'System'])
+                call_cl(run_trjconv, pipe_args=[b'MainChain', b'System'])
 
 if __name__ == '__main__':
     main()

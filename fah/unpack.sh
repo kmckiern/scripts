a414351a83197617ff8180b1fdf4499b29386847
kmckiern <kmckiern@stanford.edu>
2017-02-16 10:11:18 -0800

    clean up

--- fah/unpack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# unpack fah output zip dirs
-while read TRJ; do tar xvfj $TRJ; done < ../lasts.dat
--- opioid/check_rst.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat $1 | while read LINE; do
-    LIG=$(echo $LINE | awk -F '/' '{print $1}')
-    echo $LIG
-    ls -ltrh $LINE
-    tail -n 3 $LINE
-done
